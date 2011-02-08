f2b45bfd78214494e85416949bccb254a3653760
unknown <Administrator@.xmos.local>
2011-02-08 11:31:30 +0000

    Changes made for regression

--- plugin_pwm/src/PWM.cpp
+++ plugin_pwm/src/PWM.cpp
@@ -8,15 +8,11 @@
 #include "PwmMonitor.h"
 //#include "XmosSyscalls.h"
 
-#define NOTIFY_PLUGINS_START_TRACE 0
-#define NOTIFY_PLUGINS_STOP_TRACE  1
-
-#define NOTIFY_PWM_PLUGIN_DONE_ADDRESS 1000
-#define NOTIFY_PWM_PLUGIN_MAX_NUM_CYCLES 1001
+#define START_MONITORING 1000
+#define STOP_MONITORING  1001
+#define REPORT_STATUS    1002
 
 #define MONITOR(x) ((PwmMonitor *)x)
-
-//using namespace std;
 
 static unsigned int s_numPluginInstances = 0;
 
@@ -27,7 +23,7 @@ XsiStatus pluginCreate(void **instance, XsiCallbacks *xsi, const char *arguments
 		return XSI_STATUS_INVALID_ARGS;
 	}
 
-	*instance = (void *)new PwmMonitor(xsi, s_numPluginInstances, arguments);
+	*instance = (void *)new PwmMonitor(xsi, arguments);
 	++s_numPluginInstances;
     return XSI_STATUS_OK;
 }
@@ -44,20 +40,16 @@ XsiStatus pluginClock(void *instance) {
 
 XsiStatus pluginNotify(void *instance, int type, unsigned arg1, unsigned arg2) {
     switch (type) {
-	case NOTIFY_PLUGINS_START_TRACE:
-		MONITOR(instance)->setTracingEnabled(true);
+	case START_MONITORING:
+		MONITOR(instance)->setMonitoring(true);
 	    break;
 
-    case NOTIFY_PLUGINS_STOP_TRACE:
-		MONITOR(instance)->setTracingEnabled(false);
+    case STOP_MONITORING:
+		MONITOR(instance)->setMonitoring(false);
         break;
 
-    case NOTIFY_PWM_PLUGIN_DONE_ADDRESS:
-    	MONITOR(instance)->setDoneAddress(arg1);
-    	break;
-
-    case NOTIFY_PWM_PLUGIN_MAX_NUM_CYCLES:
-    	MONITOR(instance)->setMaxNumCycles(arg1);
+    case REPORT_STATUS:
+    	MONITOR(instance)->reportStatus();
     	break;
 
 	default:
